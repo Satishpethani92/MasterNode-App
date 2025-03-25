6273348f58ec8aefd26a8819de58ef177d0433e2
Satish pethani <satyapethani@gmail.com>
2025-03-25 18:12:00 +0530

    fixed error

--- models/blockchain/web3rpc.js
+++ models/blockchain/web3rpc.js
@@ -5,16 +5,12 @@ const config = require('config')
 
 const web3Prc = {
     Web3Rpc: function () {
-        const provider = new Web3.providers.HttpProvider(config.get('blockchain.rpc'), {
-            headers: [{ name: 'User-Agent', value: 'Node.js' }]
-        })
+        const provider = new Web3.providers.HttpProvider(config.get('blockchain.rpc'))
         const web3 = new Web3(provider)
         return web3
     },
     Web3RpcInternal: function () {
-        const internalProvider = new Web3.providers.HttpProvider(config.get('blockchain.internalRpc'), {
-            headers: [{ name: 'User-Agent', value: 'Node.js' }]
-        })
+        const internalProvider = new Web3.providers.HttpProvider(config.get('blockchain.internalRpc'))
         const web3Internal = new Web3(internalProvider)
         return web3Internal
     }
