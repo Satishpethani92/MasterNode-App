fcd57e8cd8c7912c1b42cf565f23c975bb40225b
Satish pethani <satyapethani@gmail.com>
2025-03-25 16:00:55 +0530

    Font changes done

--- models/blockchain/web3ws.js
+++ models/blockchain/web3ws.js
@@ -5,16 +5,12 @@ const config = require('config')
 
 const web3Ws = {
     Web3Ws: function () {
-        let provider = new Web3.providers.WebsocketProvider(config.get('blockchain.ws'), {
-            headers: [{ name: 'User-Agent', value: 'Node.js' }]
-        })
+        let provider = new Web3.providers.WebsocketProvider(config.get('blockchain.ws'))
         let web3 = new Web3(provider)
         return web3
     },
     Web3WsInternal: function () {
-        let provider = new Web3.providers.WebsocketProvider(config.get('blockchain.internalWs'), {
-            headers: [{ name: 'User-Agent', value: 'Node.js' }]
-        })
+        let provider = new Web3.providers.WebsocketProvider(config.get('blockchain.internalWs'))
         let web3 = new Web3(provider)
         return web3
     }
